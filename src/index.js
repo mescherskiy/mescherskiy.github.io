eb6e1baf34be27fb53f6f18697e32f7372bf905b
Alexander <mescherskiy.alexandr@gmail.com>
2023-09-07 23:32:57 +0200

    gh-pages added to package.json

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/*" element={<App />} />
+          <Route path="/mediahosting_frontend_v2/*" element={<App />} />
         </Routes>
       </BrowserRouter>
     </Provider>
